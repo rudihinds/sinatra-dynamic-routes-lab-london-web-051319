bafb4ee3296d78a5c56b47220688adc5c4ebe0f9
rudi hinds <rudihinds@gmail.com>
2019-06-04 14:02:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     @num = params[:number].to_i
     @phrase = params[:phrase]
     @num.times do
-      "#{@phrase}"
+      "#{@phrase}\n"
     end
     # @num.times { str += "#{@phrase}\n" }
     # str
