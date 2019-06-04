fa35c120cb7501ede80a8d0b41bcc7b79a600dc1
rudi hinds <rudihinds@gmail.com>
2019-06-04 13:35:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,39 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
-  
-  Skip to content
- 
-Search or jump to…
 
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@rudihinds 
- The password you provided is weak and can be easily guessed. To increase your security, please change your password as soon as possible.
-
-Read our documentation on safer password practices.
-
-9
-1 1,184 learn-co-students/sinatra-dynamic-routes-lab-v-000
- Code  Issues 0  Pull requests 1,211  Projects 0  Wiki  Security  Insights
-Done #61
- Open	Smulligan85 wants to merge 1 commit into learn-co-students:master from Smulligan85:master
-+44 −1 
- Conversation 0   Commits 1   Checks 0   Files changed 1
- Open
-Done #61
-File filter...  
-  45  app.rb 
-@@ -1,4 +1,47 @@
-require_relative 'config/environment'	require_relative 'config/environment'
-
-
- class App < Sinatra::Base	class App < Sinatra::Base
-end 	
    get '/reversename/:name' do
     @reverse_name = params[:name].reverse
     @reverse_name
